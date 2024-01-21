72436e88e2a75663c8225c5204f6504b990347c8
Andrei Tekhtelev <a.tekhtelev@gmail.com>
2024-01-21 12:52:01 -0800

    Added css for botom sheet

--- src/utils/bootstrap.js
+++ src/utils/bootstrap.js
@@ -2,3 +2,4 @@ import '../index.css';
 import 'react-mobile-cropper/dist/style.css';
 import '../components/styles/styles.css';
 import '../components/styles/swiper.css';
+import 'react-spring-bottom-sheet/dist/style.css';
